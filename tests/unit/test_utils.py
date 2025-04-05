9886f7a004f194884214677e6a44dea583407002
Kaspar Peterson <kasparpeterson@hotmail.com>
2025-04-05 11:13:48 +0100

    Adds CI with unit tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,3 +4,4 @@ from fire import utils
 def test_sha256():
     result = utils.sha256("abc")
     assert result == "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
+    assert 1 == 2
